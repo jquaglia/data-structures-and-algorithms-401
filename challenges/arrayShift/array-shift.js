0aae4fd000c51e552b1f2f240a1d672949c9df4d
jquaglia <jqugali@gmail.com>
2021-02-19 00:43:15 -0800

    completed code challenges without using methods to cheat

--- challenges/arrayShift/array-shift.js
+++ challenges/arrayShift/array-shift.js
@@ -3,8 +3,16 @@
 function insertShiftArray(array, value){
   const middle = array.length / 2;
   const middleIndex = Math.ceil(middle);
-  array.splice(middleIndex, 0, value);
-  return array;
+  const result = [];
+  for(let i = 0; i < middleIndex; i++){
+    result[result.length] = array[i];
+  }
+  result[middleIndex] = value;
+  for(let i = middleIndex + 1; i < array.length + 1; i++){
+    result[i] = array[i - 1];
+  }
+  console.log(result);
+  return result;
 }
 
 module.exports = insertShiftArray;
