41b53cf65ab0a0ac7323289d151260d4bdf702c9
jquaglia <jqugali@gmail.com>
2021-05-03 15:09:01 -0700

    looked at other solutions

--- challenges/leetCode/longestCommonPrefix/longest-common-prefix.js
+++ challenges/leetCode/longestCommonPrefix/longest-common-prefix.js
@@ -25,12 +25,39 @@ var longestCommonPrefix = function (strs) {
     }
     prefix += strs[0][i];
   }
-
   return prefix;
 };
 
-// const strs = ['dog', 'racecar', 'car'];
-// const strings = ['flower', 'flow', 'flight'];
-// console.log(longestCommonPrefix(strings));
+// var longestCommonPrefix = function (strs) {
+//   if (strs === undefined || strs.length === 0) { return ''; }
+//   //                  accu, value
+//   return strs.reduce((prev, next) => {
+//     let i = 0;
+//     while (prev[i] && next[i] && prev[i] === next[i]) i++;
+//     return prev.slice(0, i);
+//   });
+// };
+
+// var longestCommonPrefix = function (strs) {
+
+//   let lcp = '';
+//   if (!strs || !strs.length) return lcp;
+
+//   // sorting array alphabetically 
+//   strs.sort()
+
+//   for (let i = 0; i < strs[0].length; ++i) {
+//     if (strs[0][i] !== strs[strs.length - 1][i]) {    // comparing only first and last item
+//       break;
+//     } else {
+//       lcp += strs[0][i];
+//     }
+//   }
+//   return lcp;
+// };
+
+const strs = ['dog', 'racecar', 'car'];
+const strings = ['flower', 'flow', 'flowight'];
+console.log(longestCommonPrefix(strings));
 
 module.exports = longestCommonPrefix;
