a9a94f858c8bd6be6f90e497bd48b0945acd7352
jquaglia <jqugali@gmail.com>
2021-05-24 16:05:11 -0700

    completed working solution that passes all tests

--- challenges/leetCode/removeDuplicatesSortedList/remove-duplicates-sorted-list.js
+++ challenges/leetCode/removeDuplicatesSortedList/remove-duplicates-sorted-list.js
@@ -11,27 +11,63 @@ const ll = require('../../linkedList/linked-list.js');
 // Output: [1,2,3]
 
 const list = new ll.ll();
-list.head = new ll.node(1);
-list.head.next = new ll.node(1);
-list.head.next.next = new ll.node(2);
+list.head = new ll.node(0);
+list.head.next = new ll.node(0);
+list.head.next.next = new ll.node(0);
+list.head.next.next.next = new ll.node(0);
+list.head.next.next.next.next = new ll.node(0);
 
+// Working with basic test cases
+// var deleteDuplicates = function (head) {
+//     const hash = {};
+//     let current = head;
+
+//     hash[current.value] = current.value;
+
+//     while (current.next) {
+//         if (hash[current.next.value]) {
+//             current.next = current.next.next;
+//         } else if (current.next) {
+//             hash[current.next.value] = current.next.value;
+//         }
+//         current = current.next;
+//     }
+
+//     return head;
+// };
 
 var deleteDuplicates = function (head) {
-    const hash = {};
     let current = head;
 
-    hash[current.value] = current.value;
-
-    while (current.next) {
-        if (hash[current.next.value]) {
+    while (current && current.next) {
+        if (current.next.val === current.val) {
             current.next = current.next.next;
-        } else if (current.next) {
-            hash[current.next.value] = current.next.value;
+        } else {
+            current = current.next;
         }
-        current = current.next;
     }
 
     return head;
 };
+
+// var deleteDuplicates = function (head) {
+//     if (!head) { return head; }
+//     const hash = {};
+//     let current = head;
+
+//     hash[current.val] = current.val;
+
+//     while (current) {       
+//         if (current.next && hash[current.next.val]) {
+//             current.next = current.next.next;
+//         } else {
+//             hash[current.val] = current.val;
+//             hash[current.next.val] = current.next.val;
+//         }
+//         current = current.next;
+//     }
+
+//     return head;
+// };
 
 console.log(deleteDuplicates(list.head));
