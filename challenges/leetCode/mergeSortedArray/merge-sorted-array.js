3f458254f9569074bd445cc3e0955a2ce7206b44
jquaglia <jqugali@gmail.com>
2021-05-26 16:04:36 -0700

    got working solution

--- challenges/leetCode/mergeSortedArray/merge-sorted-array.js
+++ challenges/leetCode/mergeSortedArray/merge-sorted-array.js
@@ -13,6 +13,39 @@
 // Input: nums1 = [1], m = 1, nums2 = [], n = 0
 // Output: [1]
 
-var merge = function(nums1, m, nums2, n) {
-    
+// var merge = function (nums1, m, nums2, n) {
+//     nums1.splice(m, nums1.length - m);
+
+//     loop1: for (let i = 0; i < n; i++) {
+//         loop2: for (let j = 0; j <= nums1.length; j++) {
+//             if (nums2[i] <= nums1[j] || nums1[j] === undefined) {
+//                 nums1.splice(j, 0, nums2[i]);
+//                 continue loop1;
+//             }
+//             else { continue loop2; }
+//         }
+//     }
+
+//     return nums1;
+// };
+
+var merge = function (nums1, m, nums2, n) {
+    nums1.splice(m, nums1.length - m);
+
+    loop1: for (let i = 0; i < n; i++) {
+        for (let j = 0; j <= nums1.length; j++) {
+            if (nums2[i] <= nums1[j] || nums1[j] === undefined) {
+                nums1.splice(j, 0, nums2[i]);
+                continue loop1;
+            }
+            else { continue; }
+        }
+    }
+
+    return nums1;
 };
+
+
+// console.log(merge([-1, 0, 0, 3, 3, 3, 0, 0, 0], 6, [1, 2, 2], 3));
+
+// console.log(merge([0], 0, [1], 1));
