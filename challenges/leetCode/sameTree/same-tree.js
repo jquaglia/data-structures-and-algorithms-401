9a6da488ad11cb65ac86652a3ecc4bf95b199e54
jquaglia <jqugali@gmail.com>
2021-05-28 16:20:11 -0700

    added solution found online

--- challenges/leetCode/sameTree/same-tree.js
+++ challenges/leetCode/sameTree/same-tree.js
@@ -18,45 +18,77 @@
 
 const tree = require('../../tree/tree.js');
 
-// var isSameTree = function (p, q) {
-//     const pArr = [];
-//     const qArr = [];
-
-//     const preOrder = (node, arr) => {
-//         if (!node) { return arr; }
-
-//         arr.push(node.value);
-
-//         if (node.left) { preOrder(node.left, arr); }
-//         else { arr.push(null); }
-
-//         if (node.right) { preOrder(node.right, arr); }
-//         else { arr.push(null); }
-//     };
-
-//     preOrder(p.root, pArr);
-//     preOrder(q.root, qArr);
-
-//     if (pArr.length === qArr.length){
-//         for (let i = 0; i < pArr.length; i++) {
-//             if (pArr[i] !== qArr[i]) {
-//                 return false;
-//             }
-//         }
-//         return true;
-//     } else { return false; }
-// };
-
 var isSameTree = function (p, q) {
-    if (!p && !q) return true;
-    if (!p || !q) return false;
+    const pArr = [];
+    const qArr = [];
 
-    return p.value === q.value
-        && isSameTree(p.left, q.left)
-        && isSameTree(p.right, q.right);
+    const preOrder = (node, arr) => {
+        if (!node) { return arr; }
+
+        arr.push(node.value);
+
+        if (node.left) { preOrder(node.left, arr); }
+        else { arr.push(null); }
+
+        if (node.right) { preOrder(node.right, arr); }
+        else { arr.push(null); }
+    };
+
+    preOrder(p.root, pArr);
+    preOrder(q.root, qArr);
+
+    if (pArr.length === qArr.length){
+        for (let i = 0; i < pArr.length; i++) {
+            if (pArr[i] !== qArr[i]) {
+                return false;
+            }
+        }
+        return true;
+    } else { return false; }
 };
 
+// function isSameTree(p, q) {
+//     if (!p && !q) return true;
+//     if (!p || !q || p.value !== q.value) return false;
+    
+//     return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+// }
 
+// var isSameTree = function (p, q) {
+//     if (!p && !q) return true;
+//     if (!p || !q) return false;
+
+//     return p.value === q.value
+//         && isSameTree(p.left, q.left)
+//         && isSameTree(p.right, q.right);
+// };
+
+// var isSameTree = function(p, q) {
+//     const stack1 = [], stack2 = [];
+//     while (p || q || stack1.length || stack2.length) {
+//         while (p) {
+//             stack1.push(p);
+//             p = p.left;
+//         }
+//         while (q) {
+//             stack2.push(q);
+//             q = q.left;
+//         }
+//         p = stack1.pop();
+//         q = stack2.pop();
+//         if (!p && !q) {
+//             continue;
+//         }
+//         if (!p || !q || p.val !== q.val) {
+//             return false;
+//         }
+//         stack1.push(null);
+//         stack2.push(null);
+//         p = p.right;
+//         q = q.right;
+//     }
+//     return true;
+// };
 
 const aTree = new tree.BiTree();
 const bTree = new tree.BiTree();
