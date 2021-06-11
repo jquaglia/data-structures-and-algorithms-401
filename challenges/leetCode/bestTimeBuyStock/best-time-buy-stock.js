079e9a022a103aa07b39f522d8ad54ae6dcff56e
jquaglia <jqugali@gmail.com>
2021-06-11 14:11:00 -0700

    completed problem

--- challenges/leetCode/bestTimeBuyStock/best-time-buy-stock.js
+++ challenges/leetCode/bestTimeBuyStock/best-time-buy-stock.js
@@ -37,6 +37,18 @@
 //     return profit;
 // };
 
+// const maxProfit = prices => {
+//     let min = Infinity;
+//     let profit = 0;
+
+//     for (let i = 0; i < prices.length; i++) {
+
+//         if (prices[i] < min) { min = prices[i]; }
+//         else { profit = Math.max(profit, prices[i] - min); }
+//     }
+//     return profit;
+// };
+
 const maxProfit = prices => {
     let buy = Infinity;
     let profit = 0;
