bdb91594fb713168074ad2853b21110309b716a2
jquaglia <jqugali@gmail.com>
2021-07-09 11:26:16 -0700

    added more solutions

--- challenges/leetCode/implementQwithStacks/queue-with-stacks.js
+++ challenges/leetCode/implementQwithStacks/queue-with-stacks.js
@@ -88,5 +88,39 @@ MyQueue.prototype.empty = function() {
  */
 
 
+class MyQueue {
+  constructor() {
+    this.stack1 = [];
+    this.stack2 = [];
+  }
 
+  push(val) {
+    this.stack1.push(val);
+  }
+
+  pop() {
+    if (this.stack2.length) {
+      return this.stack2.pop();
+    } else {
+      while (this.stack1.length) {
+        this.stack2.push(this.stack1.pop());
+      }
+      return this.stack2.pop();
+    }
+  }
+
+  peek() {
+    if (this.stack2.length) return this.stack2[this.stack2.length - 1];
+    else {
+      while (this.stack1.length) {
+        this.stack2.push(this.stack1.pop());
+      }
+      return this.stack2[this.stack2.length - 1];
+    }
+  }
+
+  isEmpty() {
+    return this.stack1.length === 0 && this.stack2.length === 0;
+  }
+}
 
